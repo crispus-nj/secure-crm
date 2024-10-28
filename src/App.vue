62ed091463b97ef9318c1579bfe3526bb48fe62e
crispus-n <crispus@thehillcroftgroup.com>
2024-10-28 12:49:38 +0300

    added watch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+import { watch } from 'vue'
+import { userStore } from '@/stores/user'
 import { RouterView } from 'vue-router'
+import router from './router'
+
+const store = userStore()
+watch(
+  () => store.isAuthenticated,
+  newValue => {
+    if (newValue) {
+      router.push({ name: 'Dashboard' })
+    }
+  },
+  {
+    deep: true,
+  },
+)
 </script>
 
 <template>
