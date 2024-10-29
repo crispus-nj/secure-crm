60230e0bee7a98b823b614213ea2cf6dfecbb4c2
crispus-n <crispus@thehillcroftgroup.com>
2024-10-29 10:44:00 +0300

    updated users vue

--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -1,4 +1,96 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import api from '@/services/http'
+import { processHttpErrors } from '@/services/app-service.ts'
+import Loader from '@/shared/components/Loader.vue'
+
+import Button from '@/shared/ui/Button.vue'
+
+import { ref, onMounted } from 'vue'
+
+const projects = ref([])
+const loader = ref(false)
+
+onMounted(async () => {
+  loader.value = true
+  try {
+    const users = await api.get('/projects')
+    projects.value = [...users.data] // Ensure the data structure matches your expected format
+    loader.value = false
+  } catch (error) {
+    loader.value = false
+    console.log('Error fetching customers:', error) // Log any errors in the console
+    processHttpErrors(error)
+  }
+})
+</script>
+
 <template>
-  <h1>Projects list</h1>
+  <div class="bg-gray-50 min-h-screen p-8">
+    <Loader v-if="loader"></Loader>
+    <div class="bg-white rounded-lg shadow-lg p-6" v-if="projects.length > 0">
+      <div
+        class="flex flex-col md:flex-row justify-between items-start md:items-center mb-4 space-y-4 md:space-y-0"
+      >
+        <div class="flex justify-between items-center w-full">
+          <h1 class="text-2xl font-semibold">Projects</h1>
+        </div>
+        <div class="flex items-center space-x-4 w-full md:w-auto">
+          <input
+            type="text"
+            placeholder="Search"
+            class="border border-gray-300 rounded-md px-3 py-2 w-full md:w-auto"
+          />
+          <select
+            class="border border-gray-300 rounded-md px-3 py-2 text-gray-600"
+          >
+            <option>Sort by: Newest</option>
+            <option>Sort by: Oldest</option>
+          </select>
+        </div>
+      </div>
+      <!-- Table Wrapper for Horizontal Scroll -->
+      <div class="overflow-x-auto">
+        <table
+          class="w-full bg-white border border-gray-200 rounded-lg min-w-max"
+        >
+          <thead>
+            <tr class="text-left text-gray-500 text-sm uppercase">
+              <th class="px-4 py-3">Name</th>
+              <th class="px-4 py-3 hidden md:table-cell">KRA Pin</th>
+              <th class="px-4 py-3">Phone Number</th>
+              <th class="px-4 py-3 hidden lg:table-cell">Email</th>
+              <th class="px-4 py-3">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="project in projects"
+              :key="project.id"
+              class="text-gray-700"
+            >
+              <td class="border-t px-4 py-3">
+                {{ project.firstName }} {{ project.lastName }}
+              </td>
+              <td class="border-t px-4 py-3 hidden md:table-cell">
+                {{ project.kraPin }}
+              </td>
+              <td class="border-t px-4 py-3">{{ project.phoneNumber }}</td>
+              <td class="border-t px-4 py-3 hidden lg:table-cell">
+                {{ project.email }}
+              </td>
+              <td class="border-t px-4 py-3">
+                <span class="px-2 py-1 rounded-full text-xs font-semibold">
+                  {{ customer?.role?.name }}
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="flex flex-col justify-center items-center min-h-[50vh]" v-else>
+      <span class="font-bold text-blue-500 text-lg">No Project available</span>
+      <Button class="mt-5">Add Project</Button>
+    </div>
+  </div>
 </template>
--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import api from '@/services/http'
+import Loader from '@/shared/components/Loader.vue'
+
+import { ref, onMounted } from 'vue'
+
+const customers = ref([])
+const loader = ref(false)
+
+onMounted(async () => {
+  loader.value = true
+  try {
+    const users = await api.get('/users')
+    customers.value = [...users.data] // Ensure the data structure matches your expected format
+    loader.value = false
+  } catch (error) {
+    loader.value = false
+    console.log('Error fetching customers:', error) // Log any errors in the console
+  }
+})
+</script>
+
 <template>
   <div class="bg-gray-50 min-h-screen p-8">
     <div class="bg-white rounded-lg shadow-lg p-6">
@@ -5,7 +27,7 @@
         class="flex flex-col md:flex-row justify-between items-start md:items-center mb-4 space-y-4 md:space-y-0"
       >
         <div class="flex justify-between items-center w-full">
-          <h1 class="text-2xl font-semibold">All Customers</h1>
+          <h1 class="text-2xl font-semibold">Users</h1>
         </div>
         <div class="flex items-center space-x-4 w-full md:w-auto">
           <input
@@ -21,7 +43,7 @@
           </select>
         </div>
       </div>
-
+      <Loader v-if="loader"></Loader>
       <!-- Table Wrapper for Horizontal Scroll -->
       <div class="overflow-x-auto">
         <table
@@ -29,11 +51,10 @@
         >
           <thead>
             <tr class="text-left text-gray-500 text-sm uppercase">
-              <th class="px-4 py-3">Customer Name</th>
-              <th class="px-4 py-3 hidden md:table-cell">Company</th>
+              <th class="px-4 py-3">Name</th>
+              <th class="px-4 py-3 hidden md:table-cell">KRA Pin</th>
               <th class="px-4 py-3">Phone Number</th>
               <th class="px-4 py-3 hidden lg:table-cell">Email</th>
-              <th class="px-4 py-3 hidden xl:table-cell">Country</th>
               <th class="px-4 py-3">Status</th>
             </tr>
           </thead>
@@ -43,27 +64,19 @@
               :key="customer.email"
               class="text-gray-700"
             >
-              <td class="border-t px-4 py-3">{{ customer.name }}</td>
-              <td class="border-t px-4 py-3 hidden md:table-cell">
-                {{ customer.company }}
+              <td class="border-t px-4 py-3">
+                {{ customer.firstName }} {{ customer.lastName }}
               </td>
-              <td class="border-t px-4 py-3">{{ customer.phone }}</td>
+              <td class="border-t px-4 py-3 hidden md:table-cell">
+                {{ customer.kraPin }}
+              </td>
+              <td class="border-t px-4 py-3">{{ customer.phoneNumber }}</td>
               <td class="border-t px-4 py-3 hidden lg:table-cell">
                 {{ customer.email }}
               </td>
-              <td class="border-t px-4 py-3 hidden xl:table-cell">
-                {{ customer.country }}
-              </td>
               <td class="border-t px-4 py-3">
-                <span
-                  :class="
-                    customer.status === 'Active'
-                      ? 'bg-green-100 text-green-500'
-                      : 'bg-red-100 text-red-500'
-                  "
-                  class="px-2 py-1 rounded-full text-xs font-semibold"
-                >
-                  {{ customer.status }}
+                <span class="px-2 py-1 rounded-full text-xs font-semibold">
+                  {{ customer?.role?.name }}
                 </span>
               </td>
             </tr>
@@ -73,66 +86,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      customers: [
-        {
-          name: 'Jane Cooper',
-          company: 'Microsoft',
-          phone: '(225) 555-0118',
-          email: 'jane@microsoft.com',
-          country: 'United States',
-          status: 'Active',
-        },
-        {
-          name: 'Floyd Miles',
-          company: 'Yahoo',
-          phone: '(205) 555-0100',
-          email: 'floyd@yahoo.com',
-          country: 'Kiribati',
-          status: 'Inactive',
-        },
-        {
-          name: 'Ronald Richards',
-          company: 'Adobe',
-          phone: '(302) 555-0107',
-          email: 'ronald@adobe.com',
-          country: 'Israel',
-          status: 'Inactive',
-        },
-        {
-          name: 'Marvin McKinney',
-          company: 'Tesla',
-          phone: '(252) 555-0126',
-          email: 'marvin@tesla.com',
-          country: 'Iran',
-          status: 'Active',
-        },
-        {
-          name: 'Jerome Bell',
-          company: 'Google',
-          phone: '(629) 555-0129',
-          email: 'jerome@google.com',
-          country: 'Réunion',
-          status: 'Active',
-        },
-        {
-          name: 'Kathryn Murphy',
-          company: 'Microsoft',
-          phone: '(406) 555-0120',
-          email: 'kathryn@microsoft.com',
-          country: 'Curaçao',
-          status: 'Active',
-        },
-      ],
-    }
-  },
-}
-</script>
-
-<style scoped>
-/* Optional: Custom styles for finer control */
-</style>
