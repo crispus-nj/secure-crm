24a97b8926684efeb37446aaefc4a5279d0723e0
crispus-n <crispus@thehillcroftgroup.com>
2024-10-29 10:45:12 +0300

    configured toast

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import '../index.css'
 
 import 'primeicons/primeicons.css'
 
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-bootstrap.css'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(ToastPlugin)
 app.use(createPinia())
 app.use(router)
 
